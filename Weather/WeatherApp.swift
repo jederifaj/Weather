64ddfd95a0929bb90c2b3d2a1675ebc2b110e8f2
Jack Derifaj <jackd@mac.com>
2024-05-14 12:42:57 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jack Derifaj on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
